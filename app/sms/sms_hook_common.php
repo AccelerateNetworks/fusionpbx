389510ebc392c0cf6e56850ec68734cee23237d0
Finn <finn@finn.io>
2016-05-23 23:05:24 -0700

    No global debug

--- app/sms/sms_hook_common.php
+++ app/sms/sms_hook_common.php
@@ -32,7 +32,6 @@
 */
 $debug = false;
 function route_and_send_sms($from, $to, $body) {
-	  global $debug;
 		//create the even socket connection and send the event socket command
 		$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
 		if (!$fp) {
